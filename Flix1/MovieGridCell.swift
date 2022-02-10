148af955a374d25b88ebc0f332b4a7d49dc4b69f
Oespin1226 <oespinalcastillo@horizon.csueastbay.edu>
2022-02-10 00:15:34 -0800

    Flix part 2

---
+++ Flix1/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix1
+//
+//  Created by Osiel Espinal Castillo on 2/9/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+
+}
